399d332a5cf33940903bf1cea320f46d5312235e
Mimi Schatz <mcschatz@gmail.com>
2016-04-03 21:10:49 -0400

    emails cannot have @example..com. fixed regex and tested for it

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -42,7 +42,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com, foo@barbaz..com ]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -56,10 +56,11 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-  test "email addresses should be downcased before save" do
-    @user.email = @user.email.upcase
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExampLE.CoM"
+    @user.email = mixed_case_email
     @user.save
-    assert_equal @user.email, "test@exmaple.com"
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
   test "password should be present (nonblank)" do
